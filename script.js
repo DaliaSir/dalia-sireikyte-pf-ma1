d36717084df88ffde910f0bbd1bdded5d29dd46a
Dalia Sireikyte <dalia.sireikyte@gmail.com>
2020-09-18 12:45:44 +0200

    Added script.js file

---
+++ script.js
@@ -0,0 +1,39 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "Dalia";
+
+// question 3
+
+var number = 8;
+
+// question 4
+
+var division = 20 / 5;
+
+// question 5
+
+var animal = "frog";
+typeof "frog";
+
+// question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+var orderHasShipped = true;
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// question 8
+
+for (var count = 0; count <= 9; count++) {
+  console.log(count);
+}
